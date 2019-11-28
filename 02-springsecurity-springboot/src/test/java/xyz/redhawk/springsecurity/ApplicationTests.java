f553611b7c7cd03ed5c091035e677369657a7d63
redhawk <972419185@qq.com>
2019-11-28 09:52:42 +0800

    init security

---
+++ 02-springsecurity-springboot/src/test/java/xyz/redhawk/springsecurity/ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.redhawk.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
